ba15e829da3d3b7cdbf3aa17c3e45e35de4bc679
alejnabu <ab65060@ubt-uni.net>
2024-05-15 14:49:25 +0200

    test2

---
+++ server/hsm-lab1/Controllers/test.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using hsm_lab1.Database;
+using hsm_lab1.Models;
+
+namespace hsm_lab1.Controllers
+{
+
+    public class test
+    {
+        int nr;
+    }
+}
